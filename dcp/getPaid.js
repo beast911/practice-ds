8b9dc26da21be9cb4809f3501d73cb1d5fa7a053
Kanwar <ks2591@gmail.com>
2021-10-28 23:25:32 +0800

    add code

--- dcp/getPaid.js
+++ dcp/getPaid.js
@@ -9,4 +9,27 @@ Given an array representing a line of seats of employees at MegaCorp, determine 
 For example, given [10, 40, 200, 1000, 60, 30], you should return [1, 2, 3, 4, 2, 1].
  */
 
-const getPaid = (list) => { }
+const getPaid = (list) => {
+    let sol = [];
+    sol[0] = 1;
+    for (let i = 1; i < list.length; i++) {
+        if (list[i] > list[i - 1]) {
+            sol[i] = sol[i - 1] + 1;
+        } else {
+            sol[i] = 1;
+        }
+    }
+    sol[list.length - 1] = 1;
+    for (let i = list.length - 2; i > -1; i--) {
+        let nextVal;
+        if (list[i] > list[i + 1]) {
+            nextVal = sol[i + 1] + 1;
+        } else {
+            nextVal = 1;
+        }
+        sol[i] = Math.max(nextVal, sol[i]);
+    }
+    return sol;
+}
+
+console.log(getPaid([10000000000000, 400000000, 200000, 10000, 1000, 60, 30]));
---
+++ leetcode/maxDistanceToClosest.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} seats
+ * @return {number}
+ */
+var maxDistToClosest = function (seats) {
+    let prev = -1; let right = 0;
+    let res = 0;
+    for (let i = 0; i < seats.length; i++) {
+        if (seats[i]) {
+            prev = i;
+        } else {
+            let left = i;
+            let right = i;
+            while (right < seats.length) {
+                if (seats[right]) {
+                    break;
+                }
+                right++;
+            }
+            let lDist = prev === -1 ? seats.length : left - prev;
+            let rDist = right === seats.length ? seats.length : right - i;
+            res = Math.max(res, Math.min(lDist, rDist));
+        }
+    }
+    return res;
+};
+
+console.log(maxDistToClosest([1, 0, 0, 0]));
