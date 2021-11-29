2ebbe3d69f8c7b8b19882cdbf85f0509a5dcc480
Kanwar <ks2591@gmail.com>
2021-11-29 15:39:18 +0800

    add code

--- leetcode/minWindow.js
+++ leetcode/minWindow.js
@@ -1,6 +1,64 @@
+function checkPatternExists(testStrMap, patternMap) {
+  for (let i = 0; i < patternMap.length; i++) {
+    if (patternMap[i] !== 0 && testStrMap[i] < patternMap[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 /**
  * @param {string} s
  * @param {string} t
  * @return {string}
  */
-var minWindow = function (s, t) {};
+var minWindow = function (str, pattern) {
+  if (str.length < pattern.length) {
+    return '';
+  }
+  /* const str = s.toLowerCase();
+  const pattern = t.toLowerCase(); */
+  let minWin = Number.MAX_SAFE_INTEGER;
+  let finalStr = '';
+
+  const patternMap = new Array(52).fill(0);
+  [...pattern].forEach((char, idx) => {
+    const charIdx = char.charCodeAt() - 'A'.charCodeAt();
+    patternMap[charIdx]++;
+  });
+  const matchMap = new Array(52).fill(0);
+
+  let start = 0;
+  let end = 0;
+
+  while (end < str.length) {
+    const currCharIdx = str[end].charCodeAt() - 'A'.charCodeAt();
+    matchMap[currCharIdx]++;
+    // console.log(matchMap, patternMap)
+    if (checkPatternExists(matchMap, patternMap)) {
+      let lastStart = start;
+      // console.log(start, end);
+      const val = str.substring(start, end + 1);
+      console.log(start, end);
+      if (end - start < minWin) {
+        minWin = end - start;
+        finalStr = val;
+      }
+      // console.log(val);
+      while (start <= end) {
+        if (
+          pattern.indexOf(str[start]) !== -1 &&
+          str[start] !== str[lastStart]
+        ) {
+          break;
+        }
+        matchMap[str[start].charCodeAt() - 'A'.charCodeAt()]--;
+        start++;
+      }
+    }
+    end++;
+  }
+  return finalStr;
+};
+
+console.log(minWindow('adobecodebancbbcaa', 'abc'));
